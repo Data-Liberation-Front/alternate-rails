162350d31183ea101592551dbc11ed14a12254cf
pezholio <pezholio@gmail.com>
2013-08-09 14:37:19 +0100

    Send Content-Location headers if no extension has been provided

--- lib/alternate_rails/controller_helpers.rb
+++ lib/alternate_rails/controller_helpers.rb
@@ -1,9 +1,21 @@
 module AlternateRails
   module ControllerHelpers
 
-    def set_alternate_formats(formats)
+    def alternate_formats(formats)
       @alternate_formats = formats
+      send_headers
     end
-  
+    
+    def send_headers
+      if params[:format].nil? && request.headers['action_dispatch.request.accepts']
+        format = request.headers['action_dispatch.request.accepts'].first
+        params[:format] = format.ref
+        params[:only_path] = true
+        if @alternate_formats.include? params[:format]
+          headers['Content-Location'] = url_for params
+        end
+      end
+    end
+      
   end
 end
